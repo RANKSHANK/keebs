5c09ed7608dd452e8b9862de705c36197fd6a562
rankshank <imapsngamer@gmail.com>
2025-07-18 14:23:28 +1000

    dactyl-dongle: init

--- flake.nix
+++ flake.nix
@@ -10,29 +10,39 @@
 
   outputs = { self, nixpkgs, zmk-nix }: let
     forAllSystems = nixpkgs.lib.genAttrs (nixpkgs.lib.attrNames zmk-nix.packages);
+    lib = nixpkgs.lib;
+    inherit (lib) flatten;
   in {
-    packages = forAllSystems (system: rec {
-      default = firmware;
-
-      firmware = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
-        name = "firmware";
-
-        src = nixpkgs.lib.sourceFilesBySuffices self [ ".conf" ".keymap" ".dtsi" ".yml" ".shield" ".overlay" ".defconfig" ];
-
-        board = "nice_nano_v2";
-        shield = "dactyl_manuform_5x6_%PART%";
-
-        zephyrDepsHash = "sha256-qR3b0ooc1eXZ7r4OKGqFAqb2yH8novXFVRlkyE166dc=";
+    packages = forAllSystems (system: let
+        zmk = zmk-nix.legacyPackages.${system};
 
         meta = {
           description = "ZMK firmware";
           license = nixpkgs.lib.licenses.mit;
           platforms = nixpkgs.lib.platforms.all;
         };
+
+        zephyrDepsHash =
+            "sha256-wHnW1cpwmkCePq3n0SarpfdJTg+fJqH+idLePD2766A=";
+            # lib.fakeHash;
+
+        src = nixpkgs.lib.sourceFilesBySuffices self [ ".board" ".cmake" ".conf" ".defconfig" ".dts" ".dtsi" ".json" ".keymap" ".overlay" ".shield" ".yml" "_defconfig" ];
+
+    in rec {
+
+      dactyl-5x6-dongle = zmk.buildKeyboard {
+        inherit meta src zephyrDepsHash;
+        board = "nice_nano_v2";
+        shield = "dactyl_manuform_5x6_dongle dongle_display";
       };
 
-      flash = zmk-nix.packages.${system}.flash.override { inherit firmware; };
-      update = zmk-nix.packages.${system}.update;
+      dactyl-5x6-peripherals = zmk.buildSplitKeyboard {
+        inherit meta src zephyrDepsHash;
+        name = "dactyl-5x6-peripherals";
+        board = "nice_nano_v2";
+        shield = "dactyl_manuform_5x6_%PART%";
+      };
+      inherit (zmk-nix.packages.${system}) update;
     });
 
     devShells = forAllSystems (system: {
